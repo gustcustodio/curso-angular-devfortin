6556dad0ebcc2ad7b02ba55f0ea180235221e546
gustcustodio <gustcustodio@gmail.com>
2025-12-23 09:18:05 -0300

    feat: criação de um componente pt2

--- componentes-e-estilizacoes/projeto-components/src/app/app.component.ts
+++ componentes-e-estilizacoes/projeto-components/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./card/card.component";
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, CardRoxoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
