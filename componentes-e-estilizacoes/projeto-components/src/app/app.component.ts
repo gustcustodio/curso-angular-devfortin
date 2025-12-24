9a8f6ad5caf93d1d750e07b5e1ebbbf145e0d6e3
gustcustodio <gustcustodio@gmail.com>
2025-12-24 09:31:56 -0300

    feat: utilização de componentes de outros módulos

--- componentes-e-estilizacoes/projeto-components/src/app/app.component.ts
+++ componentes-e-estilizacoes/projeto-components/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from "./card/card.component";
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
-import { CardButtonComponent } from './card-button/card-button.component';
+import { CardsModule } from './cards/cards.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent, CardRoxoComponent],
+  imports: [RouterOutlet, CardsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'projeto-components';
---
+++ componentes-e-estilizacoes/projeto-components/src/app/cards/cards.module.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardComponent } from '../card/card.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CardComponent,
+    CardButtonComponent,
+    CardRoxoComponent,
+    CardRoxoButtonComponent,
+  ],
+  exports: [
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent,
+  ],
+})
+export class CardsModule {}
