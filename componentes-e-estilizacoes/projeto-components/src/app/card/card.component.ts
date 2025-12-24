258a2c4d346227b762589da123ad0c52d269a1b5
gustcustodio <gustcustodio@gmail.com>
2025-12-24 09:51:03 -0300

    feat: utilização da interpolação

--- componentes-e-estilizacoes/projeto-components/src/app/card/card.component.ts
+++ componentes-e-estilizacoes/projeto-components/src/app/card/card.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { CardButtonComponent } from "../card-button/card-button.component";
+import { CardButtonComponent } from '../card-button/card-button.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [CardButtonComponent],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  plano = {
+    infos: {
+      tipo: 'Simples',
+      preco: '100,00',
+    },
+  };
 }
